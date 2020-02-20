33581bc1075907b828ca5093dc6dff85a77c472e
preston-tech <prestonphillips42@gmail.com>
2020-02-20 16:29:01 -0700

    added a Portfolio container

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from "./portfolio-container";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Preston Phillips Portfolio</h1>
+        <h1>Preston Phillips React Portfolio</h1>
+        <PortfolioContainer />
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
       </div>
     );
