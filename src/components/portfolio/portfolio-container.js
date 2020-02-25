c3188fe78fde4ff1a7fe6bec37ed00b79e76d184
preston-tech <prestonphillips42@gmail.com>
2020-02-25 15:47:15 -0700

    Rendering live portfolio item data from devcamp space

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,5 +1,5 @@
-
 import React, { Component } from "react";
+import axios from "axios";
 
 import PortfolioItem from "./portfolio-item";
 
@@ -10,16 +10,7 @@ export default class PortfolioContainer extends Component {
     this.state = {
       pageTitle: "Welcome to my portfolio",
       isLoading: false,
-      data: [
-        { title: "Quip", category: "eCommerce", slug: "quip" },
-        { title: "Eventbrite", category: "Scheduling", slug: "eventbrite" },
-        {
-          title: "Ministry Safe",
-          category: "Enterprise",
-          slug: "ministry-safe"
-        },
-        { title: "SwingAway", category: "eCommerce", slug: "swingaway" }
-      ]
+      data: []
     };
 
     this.handleFilter = this.handleFilter.bind(this);
@@ -33,12 +24,29 @@ export default class PortfolioContainer extends Component {
     });
   }
 
+  getPortfolioItems() {
+    axios
+      .get("https://jordan.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        this.setState({
+          data: response.data.portfolio_items
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   portfolioItems() {
     return this.state.data.map(item => {
-      return (
-        <PortfolioItem title={item.title} url={"google.com"} slug={item.slug} />
-      );
+        return (
+            <PortfolioItem title={item.name} url={item.url} slug={item.id} />
+        )
     });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
   }
 
   render() {
