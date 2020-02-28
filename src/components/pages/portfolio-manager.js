35aef367fa796c247364055b7070dbb4d541fa4e
preston-tech <prestonphillips42@gmail.com>
2020-02-28 16:37:22 -0700

    styled portfolio manager page

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 export default class PortfolioManager extends Component {
+    constructor() {
+        super ();
+
+        this.state = {
+            portfolioItems: []
+        }
+    }
+
+    getPortfolioItems() {
+        Axios.get("https://prestonphillips.devcamp.space/portfolio/portfolio_items", 
+            { withCredentials: true }
+        ).then(response => {
+            this.setState({
+                portfolioItems: [...response.data.portfolio_items]
+            })
+        }).catch(error => {
+            console.log("error in getPortfolioItems", error);
+        })
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
+
     render() {
         return (
-            <div>
-                <h1>PortfolioManager</h1>
-                <h1>PortfolioManager</h1>
+          <div className="portfolio-manager-wrapper">
+            <div className="left-column">
+              <h1>Portfolio form....</h1>
             </div>
+    
+            <div className="right-column">
+              <h1>Portfolio sidebar....</h1>
+            </div>
+          </div>
         );
+      }
     }
-}
