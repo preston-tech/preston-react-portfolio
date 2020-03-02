a6cf6fd939358541d7a04dcd372cacd704be0fdf
preston-tech <prestonphillips42@gmail.com>
2020-03-02 11:13:00 -0700

    finalizing the portfolio manager sidebar

---
+++ src/components/portfolio/portfolio-form.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class PortfolioForm extends Component {
+    render() {
+        return (
+            <div>Portfolio Form</div>
+        )
+    }
+}
