8e321993fcdf84a7b104beeeb3334a40bc59793d
preston-tech <prestonphillips42@gmail.com>
2020-03-03 10:19:16 -0700

    references are kind of broken but fixed the portfolio sidebar

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 
 import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
-import PortfolioForm from '../portfolio/portfolio-form';
+import PortfolioForm from "../portfolio/portfolio-form";
 
 export default class PortfolioManager extends Component {
   constructor() {
@@ -11,25 +11,31 @@ export default class PortfolioManager extends Component {
     this.state = {
       portfolioItems: []
     };
-    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this)
-    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this)
+
+    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(
+      this
+    );
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
   }
 
   handleSuccessfulFormSubmission(portfolioItem) {
     this.setState({
       portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
-    })
+    });
   }
 
   handleFormSubmissionError(error) {
-    console.log("handleFormSubmissionError")
+    console.log("handleFormSubmissionError error", error);
   }
 
   getPortfolioItems() {
     axios
-      .get("https://prestonphillips.devcamp.space/portfolio/portfolio_item?order_by=created_at&direction=desc", {
-        withCredentials: true
-      })
+      .get(
+        "https://jordan.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc",
+        {
+          withCredentials: true
+        }
+      )
       .then(response => {
         this.setState({
           portfolioItems: [...response.data.portfolio_items]
