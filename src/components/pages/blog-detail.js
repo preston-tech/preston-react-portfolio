94ec205e7eb38cf4400c21219a100fa2ca9cb43c
preston-tech <prestonphillips42@gmail.com>
2020-03-06 10:00:19 -0700

    created links to blog posts

---
+++ src/components/pages/blog-detail.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from "react";
+
+export default class BlogDetail extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Blog detail</h1>
+      </div>
+    );
+  }
+}
