93c83ccfcd607b40e1bc1a8b4a6de97d0f073d92
preston-tech <prestonphillips42@gmail.com>
2020-03-13 15:29:16 -0600

    building about page styles

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,27 @@
 import React from "react";
+// import profilePicture from "../../../static/assets/images/bio/headshot.jpg";
 
 export default function() {
-  return <div>About</div>;
+  return (
+  <div className="content-page-wrapper">
+    <div classname="left-column"
+    // style={{
+    //   background: "url(" + profilePicture + ") no-repeat",
+    //   backgroundSize: "cover",
+    //   backgroundPosition: "center"
+    // }}
+    />
+    <div className="right-column">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aliquam commodi illum? Necessitatibus adipisci, error odio sit sed, quisquam perspiciatis eos eveniet earum repellendus perferendis obcaecati nostrum a, animi cumque.
+      Debitis quidem blanditiis sit, quasi illum id amet ipsa! Dolore temporibus quisquam commodi nostrum. Veritatis porro consequatur dolorum, delectus ad ea, corrupti obcaecati laboriosam alias asperiores omnis, ducimus voluptatem aspernatur!
+      tio soluta ipsa quia? DeleLorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aliquam commodi illum? Necessitatibus adipisci, error odio sit sed, quisquam perspiciatis eos eveniet earum repellendus perferendis obcaecati nostrum a, animi cumque.
+      Debitis quidem blanditiis sit, quasi illum id amet ipsa! Dolore temporibus quisquam commodi nostrum. Veritatis porro consequatur dolorum, delectus ad ea, corrupti obcaecati laboriosam alias asperiores omnis, ducimus voluptatem aspernatur!
+      tio soluta ipsa quia? DeleLorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aliquam commodi illum? Necessitatibus adipisci, error odio sit sed, quisquam perspiciatis eos eveniet earum repellendus perferendis obcaecati nostrum a, animi cumque.
+      Debitis quidem blanditiis sit, quasi illum id amet ipsa! Dolore temporibus quisquam commodi nostrum. Veritatis porro consequatur dolorum, delectus ad ea, corrupti obcaecati laboriosam alias asperiores omnis, ducimus voluptatem aspernatur!
+      tio soluta ipsa quia? DeleLorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aliquam commodi illum? Necessitatibus adipisci, error odio sit sed, quisquam perspiciatis eos eveniet earum repellendus perferendis obcaecati nostrum a, animi cumque.
+      Debitis quidem blanditiis sit, quasi illum id amet ipsa! Dolore temporibus quisquam commodi nostrum. Veritatis porro consequatur dolorum, delectus ad ea, corrupti obcaecati laboriosam alias asperiores omnis, ducimus voluptatem aspernatur!
+      tio soluta ipsa quia? Deleniti sunt beatae, vel laborum eveniet quia cum unde nisi numquam officia.
+    </div>
+    </div>
+  );
 }
