af4170c361aa4dd3d28d4b1eeacd37369bdbaf7f
preston-tech <prestonphillips42@gmail.com>
2020-03-25 13:21:00 -0600

    portfolio detail styles

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -4,6 +4,10 @@ import axios from 'axios';
 export default class PortfolioDetail extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      portfolioItem: {}
+    }
   }
 
   componentWillMount() {
@@ -11,18 +15,60 @@ export default class PortfolioDetail extends Component {
   }
 
   getPortfolioItem() {
-    axios.get(`https://prestonphillips.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`, { withCredentials: true }).then(response => {
-      console.log("res", response);
-    }).catch(error => {
+    axios.get(`https://prestonphillips.devcamp.space/portfolio/portfolio_items/${
+      this.props.match.params.slug
+    }`,
+    {withCredentials: true}
+    )
+    .then(response => {
+      this.setState({
+        portfolioItem: response.data.portfolio_item
+      })
+    })
+    .catch(error => {
       console.log("getportfolioitem error", error);
-    });
+    })
   }
 
   render() {
-    return (
-      <div>
-        <h2>Portfolio Detail for {props.match.params.slug}</h2>
+    const {
+name,
+description,
+url,
+category,
+thumb_image_url,
+banner_image_url,
+logo_url
+
+    } = this.state.portfolioItem;
+
+    const bannerStyles = {
+      backgroundImage: "url(" + banner_image_url + ")",
+      backgroundSize: "cover",
+      backgroundReapeat: "no-repeat",
+      backgroundPosition: "center center"
+    };
+
+    const logoStyles = {
+      width: "200px"
+    }
+
+  return (
+    <div className="portfolio-detail-wrapper">
+      <div className="banner" style={bannerStyles}>
+        <img src={logo_url} style={logoStyles} />
       </div>
-    );
+
+      <div className="portfolio-detail-description-wrapper">
+        <div className="description">{description}</div>
+      </div>
+
+        <div className="bottom-content-wrapper">
+          <a href={url} className="site-link" target="_blank">
+            Visit {name}
+          </a>
+      </div>
+    </div>
+  );
   }
 }
